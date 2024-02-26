3af51e80013d7ecace24e849d183bf6bee26c205
Konstantinos Meletiou <konstantinos.meletiou@gmail.com>
2024-02-26 17:09:35 +0100

    Render loaded feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,14 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Konstantinos Meletiou on 26/02/2024.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}

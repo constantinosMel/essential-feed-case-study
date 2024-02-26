f2fed9084a142df27437f898619bfd4a52f1578d
Konstantinos Meletiou <konstantinos.meletiou@gmail.com>
2024-02-26 17:57:43 +0100

    Format access control order following conventions

--- EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 import EssentialFeed
 
-final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {    private var feedLoader: FeedLoader?
+public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {    private var feedLoader: FeedLoader?
     private var imageLoader: FeedImageDataLoader?
     private var tableModel = [FeedImage]()
     private var tasks = [IndexPath: FeedImageDataLoaderTask]()

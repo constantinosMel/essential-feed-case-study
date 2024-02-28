3eaaf15a2ecb4281f0148f0ddd75853789abf7a5
Konstantinos Meletiou <konstantinos.meletiou@gmail.com>
2024-02-28 23:26:49 +0100

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Konstantinos Meletiou on 28/02/2024.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Konstantinos Meletiou on 28/02/2024.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
